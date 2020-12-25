f06d73d5067f326c733b75825907301acfcc0e51
Pedro Pablo <ppablocruzcobas@gmail.com>
2020-12-25 00:01:33 -0500

    Added Angular project to be used as Front-End. Removed WebController because now the web routes are managed by Angular not Spring. Removed unnecessary config.

--- src/main/java/com/isu/reservation/config/WebConfig.java
+++ src/main/java/com/isu/reservation/config/WebConfig.java
@@ -1,29 +1,6 @@
 package com.isu.reservation.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-@Configuration
-@EnableWebMvc
-@ComponentScan(basePackages = { "com.isu.reservation" })
 public class WebConfig extends WebMvcConfigurationSupport {
-
-    @Bean
-    public InternalResourceViewResolver resolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/resources/webapp/");
-        resolver.setSuffix(".html");
-        return resolver;
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-    }
-
 }
